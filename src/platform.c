f4ed5870208dde6b579fbbb9a899d0fad686b27a
Georges-Antoine Assi <contact@me.gantoine.com>
2025-01-04 21:39:53 -0500

    correct includes for libs

--- src/platform.c
+++ src/platform.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "include/platform.h"
+#include "platform.h"
 
 // Free memory for a single firmware
 void free_firmware(RomMPlatformFirmware* firmware) {
--- src/rom.c
+++ src/rom.c
@@ -1,4 +1,4 @@
-#include "include/rom.h"
+#include "rom.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
